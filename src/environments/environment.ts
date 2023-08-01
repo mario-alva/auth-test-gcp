dcb5d7a60d0188fb75ecb0d667d227e458cc0adf
Mario Alvarado <mario.alvarado@Marios-MacBook-Pro.local>
2023-07-31 20:32:59 -0600

    Initial configuration for Authentication with GCP

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environmentConfig = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA-OU4TNdwVvIsoAkUZ8ut_Jc1Wp8bdWH8",
+    authDomain: "auth-test-gcp.firebaseapp.com",
+    projectId: "auth-test-gcp",
+    storageBucket: "auth-test-gcp.appspot.com",
+    messagingSenderId: "973064638209",
+    appId: "1:973064638209:web:777911f3c6f546336e9500",
+    measurementId: "G-DRCBWB153K"
+  }
+};
